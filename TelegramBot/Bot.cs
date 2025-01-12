d5c90443241a63320865d47e35868f5da3e471ff
Alexandr Pogorelov <alexandr.pogorelov91@gmail.com>
2025-01-12 18:32:55 +0700

    Работа над телеграм ботом

---
+++ TelegramBot/Bot.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Xsl;
+
+namespace TelegramBot
+{
+    /// <summary>
+    /// Основной класс
+    /// </summary>
+    internal class Bot
+    {
+        /// <summary>
+        /// объект, отвечающий за отправку сообщений клиенту
+        /// </summary>
+        private IBotClient _telegramClient;
+
+        public Bot(IBotClient telegramClient)
+        {
+            _telegramClient = telegramClient;
+        }
+
+        /// <summary>
+        /// Метод, регистриующий обработчеки-хандлеры
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            _telegramClient.OnMessage += HandleMessage;
+            _telegramClient.OnMessage += HandleButtonClick;
+
+            Console.WriteLine("Bot started");
+        }
+
+        /// <summary>
+        /// Обработчик входящих текстовых сообщений
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        private async Task HandleMessage(object sender, MessageEventArgs e)
+        {
+            //Бот получил входящее сообщение пользователя
+            var messageText = e.Message.Text;
+
+            //Бот отправляет ответ
+            _telegramClient.SendTextMessage(e.ChatId, "Ответ на сообщение пользователя");
+        }        
+
+        /// <summary>
+        /// Обработчик нажатий на кнопку
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        private async Task HandleButtonClick(object sender, MessageEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ VoiceTexterBot/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+
+namespace VoiceTexterBot
+{
+    internal class Program
+    {
+        public static async Task Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // Объект, отвечающий за постоянный жизненый цикл приложения
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) => ConfigureServices(services))//Задаем конфигурацию
+                .UseConsoleLifetime()//Позволяет поддерживать приложение активным в консоли
+                .Build();//Собираем
+
+            Console.WriteLine("Сервис запущен");
+            //Запускаем сервис
+            await host.RunAsync();
+            Console.WriteLine("Сервис остановлен");
+        }
+        static void ConfigureServices(IServiceCollection services)
+        {
+            //Регистрируем объект TelegramBotClient с токеном подключения
+            services.AddSingleton<ITelegramBotClient>
+                (provider => new TelegramBotClient("BOT_TOKEN"));
+            //Регистрируем постоянно активный сервис бота
+            services.AddHostedService<Bot>();
+        }
+    }
+}
