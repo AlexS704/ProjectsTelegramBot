17c287b0874e847c477f8bff91ac0cab050a2581
Alexandr Pogorelov <alexandr.pogorelov91@gmail.com>
2025-07-14 14:01:48 +0700

    Переименовано пространство имен на более подходящие логике бота. Добавлены комментарии

--- MyCounterBot/Bot.cs
+++ MyCounterBot/Bot.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.Extensions.Hosting; //библиотека для подключения постоянно активного сервиса 
+﻿
+using Microsoft.Extensions.Hosting; 
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types.Enums;
 
+//Требования:
+//1.Бот должен иметь две функции: подсчёт количества символов в тексте и вычисление суммы чисел,
+//которые вы ему отправляете (одним сообщением через пробел).
+//2.То есть в ответ на условное сообщение «сова летит» он должен прислать что-то вроде
+//«в вашем сообщении 10 символов». А в ответ на сообщение «2 3 15» должен прислать «сумма чисел: 20».
+//3. Выбор одной из двух функций должен происходить на старте в «Главном меню». При старте (через /start)
+//бот должен присылать клиенту ответное сообщение — меню с кнопками, из которого можно выбрать, какое действие
+//пользователь хочет выполнить (по аналогии с тем, как мы выбирали язык в VoiceTexterBot).
 
-namespace VoiceTexterBot
+namespace CharacterCounterBot
 {
     internal class Bot : BackgroundService
     {
@@ -19,6 +28,7 @@ namespace VoiceTexterBot
         {
             _telegramClient = telegramClient;
         }
+
         /// <summary>
         /// Метод запуска бота
         /// </summary>
@@ -35,6 +45,7 @@ namespace VoiceTexterBot
             Console.WriteLine("Бот запущен");
             return Task.CompletedTask;
         }
+
         /// <summary>
         /// Метод обработки обновлений ботом
         /// </summary>
@@ -55,6 +66,7 @@ namespace VoiceTexterBot
                     cancellationToken: cancellationToken);
                 return;
             }
+
             //Обрабатываем входящие сообщения из Telegram Bot API: https://core.telegram.org/bots/api#message
             if (update.Type == UpdateType.Message)
             {
@@ -79,7 +91,8 @@ namespace VoiceTexterBot
                 }                        
                   
             } 
-        }   
+        } 
+        
         /// <summary>
         /// Метод обработки ошибок
         /// </summary>
